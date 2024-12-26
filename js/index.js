9111cc7b01779c42817f0c3f5c424521e1d55365
Bri A <alexiellis22@gmail.com>
2024-12-26 14:23:16 -0500

    Experiment inside index.js, comment out code, update README

--- js/index.js
+++ js/index.js
@@ -1,4 +1,55 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+"use strict";
 
+/* console.log(1 + "1");         // Output: 11
+                              // Answer: number 1 is converted to a string 
+                              // and the two are concatenated.
+                              // (Strings are converted by parsing them
+                              // as if they contain a number literal.) 
+
+let fifty = "50";
+let four = 4;
+console.log(fifty + four);
+
+function isEven(num) {
+  return num % 2 === 0;  //  Example for modulus use:
+                         // Even number returns a remainder of 0
+                         //  Odd number returns a remainder of 1
+}
+
+console.log(isEven(11)); // Output: false
+console.log(isEven(40)); // Output: true
+
+console.log(1 == "1");   // Output: true  - number 1 is converted to string
+console.log(1 === "1");  // Output: false  -Compares both value and type
+
+
+const cats = 4;
+const two = 2;
+
+const catTotal = cats ** two; 
+
+console.log(catTotal); // Output: Exponent 16
+
+let num1 = 23;
+let num2 = 33; 
+
+let result = num2 <= num2;
+console.log(result); //   Output: true
+
+console.log(1 || 2);         // Output: 1 1st truthy value encountered 
+console.log(2 || 1);         // Output: 2
+                            // falsy values: false, 0, -0, 0n, ""
+                            // null, undefined, NaN
+
+let mammal = "panda";
+let bird = "crow";
+let reptile = "snake";
+
+                            
+console.log(mammal && bird);         //Output: crow (Since both mammal and bird are truthy,
+                                    // the AND operator returns the second 
+                                    // operand, which is the value of bird, i.e., "crow")
+console.log(bird|| reptile);       // Output: Cow
+ */
